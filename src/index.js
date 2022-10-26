fe9f430e20be09a62a7872ce03cc263f88b4c4f0
ZangTengfei <tengfei.zang@compass.com>
2022-10-26 21:56:58 +0800

    init the interaction

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-const main = () => {
-  console.log('Hello World!');
+import inquirer from 'inquirer';
+
+const main = async () => {
+  const answerOne = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'question',
+      message: '请输入你要分析的 sourceMap 文件名',
+      default: 'bundle.js.map.json',
+    },
+  ]);
+  const answerTwo = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'question',
+      message: '请输入错误信息所在的行',
+      default: '118',
+    },
+  ]);
+  const answerThree = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'question',
+      message: '请输入错误信息所在的列',
+      default: '57',
+    },
+  ]);
+
+  // TODO: use user's answer to figure out the actual file and code line
+
+  console.log('你要查找的文件是：', answerOne.question);
+  console.log('出错的行数是：', answerTwo.question);
+  console.log('出错的列数是：', answerThree.question);
 };
 
 main();
